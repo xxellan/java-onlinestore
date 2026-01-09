6ae095f91ff4308c200d6e2203e4ebfa5c28f3f3
chetverikova.valeriya <chetverikova.valerka@gmail.com>
2026-01-09 22:46:37 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,20 @@
 public class Main {
   public static void main(String[] args) {
-    // Создаем товары
     Product laptop = new Product("Ноутбук", 75000.0);
     Product mouse = new Product("Мышка", 1500.0);
     Product keyboard = new Product("Клавиатура", 3000.0);
 
-    // Создаем корзину
     Cart cart = new Cart();
 
-    // Добавляем товары
     cart.addProduct(laptop);
     cart.addProduct(mouse);
     cart.addProduct(keyboard);
 
-    // Печатаем корзину
     cart.printCart();
 
-    // Удаляем товар
     System.out.println("Удаляем: Мышка");
     cart.removeProduct("Мышка");
 
-    // Печатаем корзину снова
     cart.printCart();
   }
 }
