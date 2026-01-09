b153a375dd98948776181b8adff54da8aea8c655
chetverikova.valeriya <chetverikova.valerka@gmail.com>
2026-01-09 22:44:45 +0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,13 +1,11 @@
 public class Product {
     private String name;
     private double price;
-    // Конструктор
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    // Геттеры
     public String getName() {
         return name;
     }
@@ -16,10 +14,8 @@ public class Product {
         return price;
     }
 
-    // Красивый вывод
     @Override
     public String toString() {
         return name + " — " + price + " руб.";
     }
 }
-
